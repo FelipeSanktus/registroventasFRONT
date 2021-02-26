210e406c8298cce48a5b900c04a08c2a51de4c7e
Felipe Sanchez Ortiz <felipe.sanchez.o@usach.cl>
2021-02-26 04:15:27 -0300

    Se la estilización de navbar

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Navbar from '../components/Navbar/Navbar';
 
 function Products() {
   return (
+    <div>
+    <Navbar />
     <div className='products'>
       <h1>Products</h1>
     </div>
+    </div>
+   
   );
 }
 
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,7 +2,9 @@ import React from 'react'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Login from '../pages/Login'
 import Menu from '../pages/Menu'
-import Navbar from '../components/Navbar/Navbar';
+import Products from '../pages/Products';
+//import Navbar from '../components/Navbar/Navbar';
+
 function Routes() {
   return (
     <BrowserRouter>
@@ -10,6 +12,7 @@ function Routes() {
       <Switch>
         <Route exact path="/" component={Login}/>
         <Route exact path="/menu" component={Menu}/>
+        <Route exact path="/products" component={Products}/>
       </Switch>
     
     </BrowserRouter>
